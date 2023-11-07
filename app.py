1dbe6550a357de0a1b8ab49db955f037b9319bf0
yite <yite@orichal.io>
2023-11-07 17:42:54 +0800

    template

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flasgger import Swagger
 from api.views import api_blueprint
 from config import Config
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    Swagger(app)
+    Swagger(app, config=app.config["SWAGGER"], merge=True)
     app.register_blueprint(api_blueprint, url_prefix='/api')
     return app
+
 
 app = create_app()
 
--- config.py
+++ config.py
@@ -3,6 +3,15 @@ class Config:
     DEBUG = False
     SWAGGER = {
         'title': 'My API',
-        'uiversion': 3
+        'uiversion': 3,
+        'openapi': '3.0.2',
+        'specs': [
+            {
+                'endpoint': 'api',
+                'route': '/apispec.json',
+                'rule_filter': lambda rule: True,  # all in
+                'model_filter': lambda tag: True,  # all in
+            }
+        ]
     }
     # ... other config settings
